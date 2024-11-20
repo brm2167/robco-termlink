edeea3f952f11bff0875b60f7f191a16c6514609
Benjamin Metzger (RIT Student) <brm2167@rit.edu>
2024-11-20 13:02:44 +0100

    Set up curses

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import curses
 
-def main():
-    pass
+def main(stdscr: curses.window):
+    stdscr.keypad(True)
+    curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
 
-if __name__ == "__main__":
-    main()
+    top: str = "Press 'q' to quit"
+    while True:
+        stdscr.clear()
+        stdscr.addstr(0, 0, top)
+        key = stdscr.getch()
+
+        if key == ord('q'):
+            break
+        elif key == curses.KEY_MOUSE:
+            _, x, y, _, button = curses.getmouse()
+            top = f"Mouse clicked at {x}, {y} with button {button}"
+        else:
+            stdscr.addstr(1, 0, f"Key pressed: {chr(key) if key < 256 else key}")
+
+        stdscr.refresh()
+
+curses.wrapper(main)
